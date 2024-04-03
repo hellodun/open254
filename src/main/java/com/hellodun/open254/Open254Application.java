b038ea63f2bfccd6771798ca9165efc0175b6b4c
Duncan Gatuthu <dgatuthu@gmail.com>
2024-04-03 10:14:08 +0300

    initial commit

---
+++ src/main/java/com/hellodun/open254/Open254Application.java
@@ -0,0 +1,13 @@
+package com.hellodun.open254;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Open254Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Open254Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hellodun/open254/Open254ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellodun.open254;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Open254ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
